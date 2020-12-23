3f1bdfa682577cc97e354f39091213fb82e4aa71
Anil <anil_gurung15@yahoo.com>
2020-12-23 00:12:08 -0800

    check build status

--- src/index.js
+++ src/index.js
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { createStore } from 'redux'
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
 import rootReducer from './reducers'
 
 const store = createStore(rootReducer);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
